322aade7a9d977e23a01e37f1b5eaa590220327e
aflopez0408 <74578619+aflopez0408@users.noreply.github.com>
2025-07-27 08:52:58 -0500

    chore: inicializar proyecto con dependencias básicas en Spring Initializr

---
+++ app_list/src/main/java/com/andres/app_list/AppListApplication.java
@@ -0,0 +1,13 @@
+package com.andres.app_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppListApplication.class, args);
+	}
+
+}
---
+++ app_list/src/test/java/com/andres/app_list/AppListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.app_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
